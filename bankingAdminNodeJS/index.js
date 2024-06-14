fdd0c269113fd847b64894346e3d0d50958663ea
lescobar-2019578 <myac-2022020@kinal.edu.gt>
2024-06-14 13:36:51 -0600

    Se elimino backend de admin

--- bankingAdminNodeJS/index.js
+++
@@ -1,7 +0,0 @@
-import { initServer } from "./config/app.js";
-import { connect } from "./config/mongo.js";
-import { initAdmin } from './src/user/user.controller.js'
-
-initAdmin()
-initServer()
-connect()
