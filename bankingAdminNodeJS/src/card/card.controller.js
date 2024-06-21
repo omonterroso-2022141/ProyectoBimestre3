3d79dba99d5be134f39f4d83ff9ca135a940f266
myac-2022020 <myac-2022020@gmail.com>
2024-06-21 17:16:24 -0600

    Se actualizo tarjetas

--- bankingAdminNodeJS/src/card/card.controller.js
+++ bankingAdminNodeJS/src/card/card.controller.js
@@ -1,18 +1,23 @@
 'use strict';
 
-import Card from '../card/card.model.js';
+import Card from './card.model.js'
 import User from '../user/user.model.js';
+import Account from '../account/account.model.js'
 
 export const createCard = async (req, res) => {
     try {
-        let data = req.body;
-        // cardNumber, userId, dueDate, CVV, issued
-        data.userId = req.uid
-        //Se agrega la fecha de vencimiento de la tarjeta
-        let due = new Date(data.dueDate)
-        dueDate.setHours(0,0,0,0)
+        let data = req.body
+        let dataAccount = await Account.findOne(data.cardNumber)
+        if(!dataAccount) return res.status(404).send({message: ''})
+        let today = new Date();
+        let dueDate = new Date(today.getFullYear() + 4, today.getMonth(), today.getDate());
+        dueDate.setHours(0, 0, 0, 0);
+        console.log("Fecha de vencimiento dentro de 4 años:", dueDate);
+
+        return res.send({message: 'La fecha de hoy es '+today+', y la fecha de vencimiento es '+dueDate})
     } catch (err) {
         console.error(err);
         return res.status(500).send({ message: 'Error creating card' });
     }
 }
+
