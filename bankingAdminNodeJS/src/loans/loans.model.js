2b37e2161df7efa316eed1ea634814f0f957c924
dchupina-2023120 <myac-2022020@kinal.edu.gt>
2024-07-18 14:00:21 -0600

    Se actualizo la creacion de cuentas y se agrego el modelo de prestamos

--- bankingAdminNodeJS/src/loans/loans.model.js
+++ bankingAdminNodeJS/src/loans/loans.model.js
@@ -1,5 +1,24 @@
+'use strict'
+
 import { model, Schema } from 'express'
 
-const loansSchema = Schema({
-    
+const loansSchema = new Schema({
+    userId:{
+        type: Schema.ObjetcId,
+        ref: 'user',
+        required: true
+    },
+    loanType:{
+        type: Schema.ObjectId,
+        ref: 'loanType',
+        required: true
+    },
+    amount:{
+        type: Schema.Types.Decimal128,
+        required: true,
+    }
+},{
+    varsionKey: false
 })
+
+export default model('Loans', loansSchema)
